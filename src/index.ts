6a2d34b33508f70a109d48dca594ad63c161d6c6
sadhnarana17 <sadhnarana@ucreate.co.in>
2022-06-02 16:01:45 +0530

    updated main file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-// eslint-disable-next-line global-require
 import runApp from './app';
-
-require('dotenv').config();
 
 if (process.env.NEW_RELIC_LICENSE_KEY) {
   // eslint-disable-next-line global-require
@@ -12,8 +9,6 @@ if (process.env.ROLLBAR) {
   // eslint-disable-next-line global-require
   require('rollbar');
 }
-const os = require('os');
-const throng = require('throng');
 const logger = require('./service/logger').default;
 
 process.env.TZ = 'UTC';
@@ -28,42 +23,4 @@ const setupCluster = async () => {
   });
 };
 
-const concurrentImportSupported = false;
-const dynoLimitDueToConnectionPool = 20;
-
-const getNumberOfClusters = (): number => {
-  if (!concurrentImportSupported) {
-    return 1;
-  }
-
-  return Math.min(
-    Number(process.env.WEB_CONCURRENCY) || os.cpus().length,
-    dynoLimitDueToConnectionPool,
-  );
-};
-
-const clustersCount = getNumberOfClusters();
-
-const runClusters = () => {
-  logger('info', `concurrent imports supported = ${concurrentImportSupported}`);
-  logger('info', `WEB_CONCURRENCY = ${process.env.WEB_CONCURRENCY}`);
-  logger('info', `CPUs count = ${os.cpus().length}`);
-
-  if (clustersCount === 1) {
-    logger('info', 'Running app without clusters');
-    setupCluster();
-    return;
-  }
-
-  const setupMaster = () => {
-    logger('info', `Running app with ${clustersCount} clusters`);
-  };
-
-  throng({
-    worker: setupCluster,
-    master: setupMaster,
-    count: clustersCount,
-  });
-};
-
-runClusters();
+setupCluster();
